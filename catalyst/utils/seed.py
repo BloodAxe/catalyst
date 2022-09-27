6e963a2412db69663963f6e855571e2f2ef2dabd
Ievgen Khvedchenia <ekhvedchenya@gmail.com>
2022-09-27 12:49:43 +0300

    Do not import tensorflow

--- catalyst/utils/seed.py
+++ catalyst/utils/seed.py
@@ -1,7 +1,6 @@
 import random
 
 import numpy as np
-from packaging.version import parse, Version
 
 
 def set_global_seed(seed: int) -> None:
@@ -17,17 +16,7 @@ def set_global_seed(seed: int) -> None:
     else:
         torch.manual_seed(seed)
         torch.cuda.manual_seed_all(seed)
-    try:
-        import tensorflow as tf
-    except ImportError:
-        pass
-    else:
-        if parse(tf.__version__) >= Version("2.0.0"):
-            tf.random.set_seed(seed)
-        elif parse(tf.__version__) <= Version("1.13.2"):
-            tf.set_random_seed(seed)
-        else:
-            tf.compat.v1.set_random_seed(seed)
+
     random.seed(seed)
     np.random.seed(seed)
 
